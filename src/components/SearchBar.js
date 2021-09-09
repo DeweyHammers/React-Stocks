cc1a4a547eab98f562e44ae54801dfb09a0673b7
dewey <hammersdrichard@gmail.com>
2021-09-09 09:41:37 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({
+  filterStocks,
+  filterAlphabetically,
+  filterPrice,
+  checkBoxs,
+}) => {
   return (
     <div>
-
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input
+          type="radio"
+          value="Alphabetically"
+          checked={checkBoxs.alph}
+          onChange={() => filterAlphabetically()}
+        />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input
+          type="radio"
+          value="Price"
+          checked={null}
+          onChange={() => filterPrice()}
+        />
         Price
       </label>
-      <br/>
+      <br />
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(event) => filterStocks(event.target.value)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
         </select>
       </label>
-
-
     </div>
   );
-}
-
+};
 
 export default SearchBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,90 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    filter: [],
+    profile: [],
+    checkBoxs: { alph: false, price: false },
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3001/stocks")
+      .then((resp) => resp.json())
+      .then((json) => this.setState({ stocks: json }));
+  }
+
+  filterStocks = (search) => {
+    const stocks = this.state.stocks.filter((stock) => stock.type === search);
+    this.setState({ filter: stocks });
+  };
+
+  addStock = (stock) => {
+    const check = this.state.profile.filter((s) => s.id === stock.id);
+    console.log(check);
+    if (check.length === 0) {
+      this.setState({ profile: [...this.state.profile, stock] });
+    } else {
+      this.setState({
+        profile: [...this.state.profile.filter((s) => s.id !== stock.id)],
+      });
+    }
+  };
+
+  filterAlphabetically = () => {
+    const sorted = this.state.filter.sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({ filter: sorted });
+    if (this.state.checkBoxs.alph === false) {
+      this.setState({ checkBoxs: { ...this.state.checkBoxs, alph: true } });
+    }
+    console.log("clicked");
+  };
+
+  filterPrice = () => {
+    const sorted = this.state.filter.sort((a, b) => a.price - b.price);
+    this.setState({ filter: sorted });
+  };
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          filterStocks={this.filterStocks}
+          filterAlphabetically={this.filterAlphabetically}
+          filterPrice={this.filterPrice}
+          checkBoxs={this.state.checkBoxs}
+        />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className="row">
+          <div className="col-8">
+            <StockContainer
+              addStock={this.addStock}
+              stocks={this.state.filter}
+            />
           </div>
+          <div className="col-4">
+            <PortfolioContainer
+              addStock={this.addStock}
+              stocks={this.state.profile}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
